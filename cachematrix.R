0062decb6d6a0cdfa196188709e84690af1b2fe3
mogrein <mogrein@MacBook-Pro-Aleksandr.local>
2014-05-25 21:35:00 +0400

    Sollution
    Documentary and final solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function stores input matrix in the cache and creates structure with 
+## several functions in it:
+## get()        - function to get stored matrix
+## getinverse() - function to get inversed matrix from the cache
+## set()        - function to set new values to the stored matrix,
+##                deletes all cache with inversed matrix
+## setinverse() - function to store inversed matrix in the cache
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
+    invX <- NULL
+    #function to change matrix stored in the structure.
+    #Deletes inverted matrix cache, also.
+    set <- function(newMatrix) {
+        x <<- newMatrix
+        invX <<- NULL
     }
+
+    #function to get original matrix from the structure
     get <- function() x
-    setinverse <- function(inverse) m <<- inverse
-    getinverse <- function() m
+
+    #function to save inversed matrix in cache
+    setinverse <- function(inverse) invX <<- inverse
+
+    #function to get inversed matrix from the cache
+    getinverse <- function() invX    
+
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Function gets the structure, created by makeCacheMatrix function,
+## checkes, whether cache with inversed matrix inside this structure is empty
+##   * retrieves inversed matrix from the cache if cache is not empty,
+##   * calculates and store inversed matrix in cache if cache is empty
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-    m <- x$getinverse()
-    if(!is.null(m)) {
+    # Check the cache first
+    invX <- x$getinverse()
+    if(!is.null(invX)) {
         message("getting cached data")
-        return(m)
+        return(invX)
     }
+    
+    #No matrix in cache, calculate it instead
     data <- x$get()
-    m <- solve(data, ...)
-    x$setinverse(m)
-    m
+    invX <- solve(data, ...)
+    x$setinverse(invX)
+    invX
 }
